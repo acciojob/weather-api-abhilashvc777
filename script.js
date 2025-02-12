ac72f3a7737bac9d13ee785a6ee65e23893b55ad
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-12 15:15:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //your JS code here. If required.
 const btn = document.querySelector("button")
 const div = document.querySelector("#weatherData")
-const weatherData = async()=>{
+const getCurrentWeather = async()=>{
 	const res = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London?unitGroup=metric&key=8NBU7YB4UXCK38TRLYL7WT8Z3&contentType=json")
 	const data = await res.json()
 	btn.addEventListener("click",()=>{
 		div.textContent = `Current weather in London:${data.currentConditions.conditions}`
 	})
 }
-weatherData()
+getCurrentWeather()
