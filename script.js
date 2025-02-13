3c76347c5c4bd3afa8920497f0d0f638c84739f6
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-13 17:57:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
-const btn = document.querySelector("button")
+const btn = document.querySelector("#getWeatherBtn")
 const div = document.querySelector("#weatherData")
 let city = `London`
-	let key = `1dea75705ad20bcb9e87f9e6d630d89a`
+let key = `1dea75705ad20bcb9e87f9e6d630d89a`
 const getCurrentWeather = async()=>{
 	const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
 	const data = await res.json() 
@@ -10,12 +10,11 @@ const getCurrentWeather = async()=>{
 }
 btn.addEventListener("click",async()=>{
 	  const weatherData = await getCurrentWeather();
-		div.textContent = `Current weather in ${city} : ${weatherData.weather[0].main}`
+		div.textContent = `Current weather in ${city} : ${weatherData.weather[0].description}`
 	})
 // document.getElementById('getWeatherBtn').addEventListener('click', getWeather);
-
 // function getWeather() {
-//     const apiKey = '1dea75705ad20bcb9e87f9e6d630d89a'; // Replace 'YOUR_API_KEY' with your actual API key from OpenWeatherMap
+// 	const apiKey = '1dea75705ad20bcb9e87f9e6d630d89a'; // Replace 'YOUR_API_KEY' with your actual API key from OpenWeatherMap
 //     const city = 'London';
 //     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
