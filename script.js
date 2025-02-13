41f21cba3ebbced6bdd4f56919a087b5deb2670f
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-13 17:42:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,5 +8,22 @@ const getCurrentWeather = async()=>{
 }
 btn.addEventListener("click",async()=>{
 	  const weatherData = await getCurrentWeather();
-		div.textContent = `Current weather in London : ${weatherData.weather[0].main}`
+	  city = "London"
+		div.textContent = `Current weather in ${city} : ${weatherData.weather[0].main}`
 	})
+// document.getElementById('getWeatherBtn').addEventListener('click', getWeather);
+
+// function getWeather() {
+//     const apiKey = '1dea75705ad20bcb9e87f9e6d630d89a'; // Replace 'YOUR_API_KEY' with your actual API key from OpenWeatherMap
+//     const city = 'London';
+//     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+//     axios.get(apiUrl)
+//         .then(response => {
+//             const weatherDescription = response.data.weather[0].description;
+//             document.getElementById('weatherData').innerText = `Current weather in ${city}: ${weatherDescription}`;
+//         })
+//         .catch(error => {
+//             console.error('There was a problem fetching the weather data:', error);
+//         });
+// }
