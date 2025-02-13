231c4d38a6d4300660a8253d76e3f97ded1801ec
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-13 16:36:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 const btn = document.querySelector("button")
 const div = document.querySelector("#weatherData")
 const getCurrentWeather = async()=>{
-	const res = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London?unitGroup=metric&key=8NBU7YB4UXCK38TRLYL7WT8Z3&contentType=json")
+	const res = await fetch("https://api.openweathermap.org/data/2.5/weather?q=london&appid=1dea75705ad20bcb9e87f9e6d630d89a")
 	const data = await res.json()
 	return data;
 }
 btn.addEventListener("click",async()=>{
 	  const weatherData = await getCurrentWeather();
-		div.textContent = `Current weather in London : ${weatherData.currentConditions.conditions}`
+		div.textContent = `Current weather in London : ${weatherData.weather[0].main}`
 	})
